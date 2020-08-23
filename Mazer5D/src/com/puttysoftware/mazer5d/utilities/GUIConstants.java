12d8608f373fd24ce7886eb7c25852368acf52a3
Eric Ahnell <eric.ahnell@puttysoftware.com>
2020-08-23 08:36:05 -0400

    Adjust GUI size.

--- Mazer5D/src/com/puttysoftware/mazer5d/utilities/GUIConstants.java
+++ Mazer5D/src/com/puttysoftware/mazer5d/utilities/GUIConstants.java
@@ -5,7 +5,7 @@ import java.awt.Dimension;
 public class GUIConstants {
     private GUIConstants() {}
     
-    public static final int GUI_WIDTH = 700;
-    public static final int GUI_HEIGHT = 700;
-    public static final Dimension COMMAND_BUTTON = new Dimension(50, 15);
+    public static final int GUI_WIDTH = 776;
+    public static final int GUI_HEIGHT = 624;
+    public static final Dimension COMMAND_BUTTON = new Dimension(90, 15);
 }
