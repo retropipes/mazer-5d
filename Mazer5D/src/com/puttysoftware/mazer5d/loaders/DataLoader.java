ecb2cf019bf2b155f2f1e2f89cd12e3561935b96
RetroPipes <eric.ahnell@puttysoftware.com>
2022-06-20 09:14:01 -0400

    Add per-action data files.

--- Mazer5D/src/com/puttysoftware/mazer5d/loaders/DataLoader.java
+++ Mazer5D/src/com/puttysoftware/mazer5d/loaders/DataLoader.java
@@ -9,6 +9,10 @@ import com.puttysoftware.mazer5d.files.FileExtensions;
 import com.puttysoftware.mazer5d.utilities.MazeObjectActions;
 
 public class DataLoader {
+    private static MazeObjectActions[] ACTION_CACHE;
+    private static int[][] ACTION_ADDON_CACHE;
+    private static final int ACTION_ADDON_LENGTH = 32;
+
     private DataLoader() {
 	// Do nothing
     }
@@ -51,49 +55,58 @@ public class DataLoader {
 	}
     }
 
-    public static MazeObjectActions[] loadObjectActionData() {
-	try (final ResourceStreamReader rsr = new ResourceStreamReader(DataLoader.class.getResourceAsStream(
-		"/assets/data/object/actions" + FileExtensions.getInternalDataExtensionWithPeriod()))) {
-	    // Fetch data
-	    final ArrayList<MazeObjectActions> data = new ArrayList<>();
-	    String raw = "0";
-	    while (raw != null) {
-		raw = rsr.readString();
-		if (raw != null) {
-		    data.add(new MazeObjectActions(Long.parseLong(raw)));
+    public static MazeObjectActions loadObjectActionData(final int objectID) {
+	if (DataLoader.ACTION_CACHE == null) {
+	    try (final ResourceStreamReader rsr = new ResourceStreamReader(DataLoader.class.getResourceAsStream(
+		    "/assets/data/object/actions" + FileExtensions.getInternalDataExtensionWithPeriod()))) {
+		// Fetch data
+		final ArrayList<MazeObjectActions> data = new ArrayList<>();
+		String raw = "0";
+		while (raw != null) {
+		    raw = rsr.readString();
+		    if (raw != null) {
+			data.add(new MazeObjectActions(Long.parseLong(raw)));
+		    }
 		}
+		DataLoader.ACTION_CACHE = data.toArray(new MazeObjectActions[data.size()]);
+	    } catch (final IOException e) {
+		Mazer5D.logError(e);
+		return null;
 	    }
-	    return data.toArray(new MazeObjectActions[data.size()]);
-	} catch (final IOException e) {
-	    Mazer5D.logError(e);
-	    return null;
 	}
+	return DataLoader.ACTION_CACHE[objectID];
     }
 
-    public static int[] loadObjectActionAddonData(final int actionID) {
-	final String name = "action-" + Integer.toString(actionID);
-	try (final ResourceStreamReader rsr = new ResourceStreamReader(DataLoader.class.getResourceAsStream(
-		"/assets/data/object/" + name + FileExtensions.getInternalDataExtensionWithPeriod()))) {
-	    // Fetch data
-	    final ArrayList<Integer> rawData = new ArrayList<>();
-	    String raw = "0";
-	    while (raw != null) {
-		raw = rsr.readString();
-		if (raw != null) {
-		    rawData.add(Integer.parseInt(raw));
-		}
-	    }
-	    int index = 0;
-	    final int[] data = new int[rawData.size()];
-	    for (final Integer rawItem : rawData) {
-		data[index] = rawItem;
-		index++;
-	    }
-	    return data;
-	} catch (final IOException e) {
-	    Mazer5D.logError(e);
-	    return null;
+    public static int loadObjectActionAddonData(final int objectID, final int actionID) {
+	if (DataLoader.ACTION_ADDON_CACHE == null) {
+	    DataLoader.ACTION_ADDON_CACHE = new int[DataLoader.ACTION_ADDON_LENGTH][];
 	}
+	if (DataLoader.ACTION_ADDON_CACHE[actionID] == null) {
+	    final String name = "action-" + Integer.toString(actionID);
+	    try (final ResourceStreamReader rsr = new ResourceStreamReader(DataLoader.class.getResourceAsStream(
+		    "/assets/data/object/" + name + FileExtensions.getInternalDataExtensionWithPeriod()))) {
+		// Fetch data
+		final ArrayList<Integer> rawData = new ArrayList<>();
+		String raw = "0";
+		while (raw != null) {
+		    raw = rsr.readString();
+		    if (raw != null) {
+			rawData.add(Integer.parseInt(raw));
+		    }
+		}
+		int index = 0;
+		final int[] data = new int[rawData.size()];
+		for (final Integer rawItem : rawData) {
+		    data[index] = rawItem;
+		    index++;
+		}
+		DataLoader.ACTION_ADDON_CACHE[actionID] = data;
+	    } catch (final IOException e) {
+		Mazer5D.logError(e);
+		return 0;
+	    }
+	}
+	return DataLoader.ACTION_ADDON_CACHE[actionID][objectID];
     }
 
     public static String[] loadEffectImageData() {
