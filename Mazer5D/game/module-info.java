9605a73b81c565516904ef4631adac00ff0ff1f8
RetroPipes <eric.ahnell@puttysoftware.com>
2022-06-26 10:23:55 -0400

    Integrate more libraries.

--- Mazer5D/game/module-info.java
+++ Mazer5D/game/module-info.java
@@ -2,11 +2,6 @@ module com.puttysoftware.mazer5d {
     requires com.github.vorbis;
     requires java.desktop;
     requires com.puttysoftware.audio.ogg;
-    requires com.puttysoftware.audio.wav;
-    requires com.puttysoftware.fileutils;
-    requires com.puttysoftware.integration;
-    requires com.puttysoftware.randomrange;
-    requires com.puttysoftware.updater;
 
     uses javax.sound.sampled.spi.AudioFileReader;
     uses javax.sound.sampled.spi.FormatConversionProvider;
