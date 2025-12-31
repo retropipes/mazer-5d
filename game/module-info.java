1b3e3cf735ed5f7c92097c4faf84763efb89be86
RetroPipes <eric.ahnell@puttysoftware.com>
2025-12-31 09:50:34 -0500

    Move assets into separate source folder.

--- game/module-info.java
+++ game/module-info.java
@@ -1,5 +1,6 @@
 module com.puttysoftware.mazer5d {
     requires java.desktop;
     requires org.retropipes.diane;
+    requires org.retropipes.diane.fileio;
     requires org.retropipes.diane.internal;
 }
