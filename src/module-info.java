63ff50fb788a4e5c659ed178862364da497a29c4
RetroPipes <eric.ahnell@puttysoftware.com>
2026-01-09 12:27:13 -0500

    Update module dependencies.

--- src/module-info.java
+++ src/module-info.java
@@ -3,4 +3,7 @@ module org.retropipes.mazer5d {
     requires org.retropipes.diane;
     requires org.retropipes.diane.fileio;
     requires org.retropipes.diane.internal;
+
+    uses javax.sound.sampled.spi.AudioFileReader;
+    uses javax.sound.sampled.spi.FormatConversionProvider;
 }
